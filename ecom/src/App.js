6ca2fcaf219696998ecdc61c7302af5c753fc2ce
swati-coder-boot <swatirkudalli@gmail.com>
2021-01-19 00:49:33 -0800

    react-hook-form

--- ecom/src/App.js
+++ ecom/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-// import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 // import Swati from "./formhandlings/Swati";
 // import ClassProps from "./ClassProp";
 // import ClassState from "./ClassState";
 // import Event from "./React-Event/Event"
 // import Event1 from "./React-Event/Evevnt1";
-import Argument from "./React-Event/Argument";
+// import Argument from "./React-Event/Argument";
+import Login from "./React-Hook-Form/HookForm";
 
 
 
@@ -23,7 +24,8 @@ function App() {
         // <ClassState />
           // <Event Roll="101"/>
           // <Event1 />
-          <Argument />
+          // <Argument />
+          <Login />
       }
     </div>
 
---
+++ ecom/src/React-Hook-Form/HookForm.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+let Login = () => {
+  let { register, handleSubmit, errors } = useForm();
+
+  let onSubmit = (data) => {
+    console.log(data);
+  };
+
+
+return (
+  <React.Fragment>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6">
+          <h1> React-Hook-Form</h1>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="form-group">
+              <input ref={register({ required: true })} name="email" placeholder="email" className="form-control"/>
+              {errors.email && (<span className="text-warning">
+                {" "}
+                "Email is required swati"
+              </span>
+              )}
+            </div>
+            <div className="form-group">
+              <input ref={register} name="password" palceholder="password" className="form-control"/>
+            </div>
+            <div className="form-group">
+              <input className="btn btn-success" type="submit" />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </React.Fragment>
+)
+};
+
+export default Login;
